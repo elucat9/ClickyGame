83d91eeca18521c1aaa98c137a51550e50110616
elucat9 <elucat9@gmail.com>
2019-03-30 14:38:40 -0700

    Updates

---
+++ precache-manifest.2c15a9658e65b337a9431a9b2bbb0979.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "95fffdb9eb36913c2817",
+    "url": "/ClickyGame/static/js/runtime~main.1eb9bb0a.js"
+  },
+  {
+    "revision": "2e38490268f86c08d2d8",
+    "url": "/ClickyGame/static/js/main.7e959e69.chunk.js"
+  },
+  {
+    "revision": "00063f64c68494124901",
+    "url": "/ClickyGame/static/js/2.49afd6f5.chunk.js"
+  },
+  {
+    "revision": "2e38490268f86c08d2d8",
+    "url": "/ClickyGame/static/css/main.8c9c12f7.chunk.css"
+  },
+  {
+    "revision": "19e68218c75f4d5027bed0e404fa5a3d",
+    "url": "/ClickyGame/index.html"
+  }
+];
